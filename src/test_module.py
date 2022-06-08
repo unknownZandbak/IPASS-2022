6ac2a8eac2fb27e7b300ca230e56be8c146a0df9
UnknownZandbak <rjwillard01@gmail.com>
2022-06-08 19:52:11 +0200

    changed matrix of test rule3_1

--- src/test_module.py
+++ src/test_module.py
@@ -20,7 +20,7 @@ class TestRuleChecking(unittest.TestCase):
         self.assertEqual(main.rule_checker(matrix)[0], 4)
 
     def test_rule3_1(self):
-        matrix = np.array(([1,1,0,1],[0,1,1,0],[0,0,1,1],[1,0,0,1]))
+        matrix = np.array(([1,1,0,1,0,0],[1,0,0,1,1,0],[0,1,1,0,1,1],[1,0,1,1,0,0],[0,1,0,1,0,1],[0,0,1,0,1,1]))
         self.assertEqual(main.rule_checker(matrix)[0], 5)
     
     def test_rule3_2(self):

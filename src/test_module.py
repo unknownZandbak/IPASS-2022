2950b18f2f85f889fd5043e9f3d0a8b37bf702f1
UnknownZandbak <rjwillar01@gmail.com>
2022-06-14 14:52:48 +0200

    i now have correct test cases for rule checking

--- src/test_module.py
+++ src/test_module.py
@@ -24,7 +24,7 @@ class TestRuleChecking(unittest.TestCase):
         self.assertEqual(main.rule_checker(matrix)[0], 5)
     
     def test_rule3_2(self):
-        matrix = np.array(([1,0,0,1],[0,1,1,0],[0,0,1,1],[1,0,0,1]))
+        matrix = np.array(([5641,6854,1,1,6784,5411],[9851,1515,8545,1,0,0],[3248,5145,2187,0,67874,0],[2414,8753,4879,1,9548,2166],[6854,0,6874,1,1,6156],[7797,0,4187,0,8756,45478]))
         self.assertEqual(main.rule_checker(matrix)[0], 6)
 
 if __name__ == '__main__' :

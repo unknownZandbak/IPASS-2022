3843ed9255a6852b287fb4fc324a1907d7b73701
UnknownZandbak <rjwillar01@gmail.com>
2022-06-15 14:53:02 +0200

    added test for making the gris editable

--- src/test_module.py
+++ src/test_module.py
@@ -27,5 +27,33 @@ class TestRuleChecking(unittest.TestCase):
         matrix = np.array(([5641,6854,1,1,6784,5411],[9851,1515,8545,1,0,0],[3248,5145,2187,0,67874,0],[2414,8753,4879,1,9548,2166],[6854,0,6874,1,1,6156],[7797,0,4187,0,8756,45478]))
         self.assertEqual(main.rule_checker(matrix)[0], 6)
 
+class TestMatrixEditing(unittest.TestCase):
+    matrix = np.array(([5588, 1, 6312, 7711], [2986, 5669, 6793, 2317], [3479, 7200, 8358, 1], [7879, 4766, 2297, 5679]))
+    def test_editMatrix(self):
+        pos = row, column = 2, 2
+        newVal = 0
+        main.editGrid(self.matrix, pos, newVal)
+        self.assertEqual(self.matrix[pos], newVal)
+    
+    def test_reassignVal(self):
+        pos = row, column = 2, 2
+        newVal = 1
+        main.editGrid(self.matrix, pos, newVal)
+        self.assertEqual(self.matrix[pos], newVal)
+    
+    def test_NoneEditable(self):
+        pos = row, column = 1, 1
+        currentVal = self.matrix[pos]
+        newVal = 0
+        main.editGrid(self.matrix, pos, newVal)
+        self.assertEqual(self.matrix[pos], currentVal)
+    
+    def test_incorrectEdit(self):
+        pos = row, column = 1, 2
+        currentVal = self.matrix[pos]
+        newVal = 4885
+        main.editGrid(self.matrix, pos, newVal)
+        self.assertEqual(self.matrix[pos], currentVal)
+
 if __name__ == '__main__' :
     unittest.main(verbosity=0)
